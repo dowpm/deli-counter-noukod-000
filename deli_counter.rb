9d3ce579c0ecb3db877e6a761a092f1a223e377a
Mac Dowell Prosper <macdowell@noukod.com>
2018-09-20 03:31:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(arr)
+  str = "The line is currently:"
+  puts "The line is currently empty." if arr.empty?
+  arr.each_with_index {|name, index| str << " #{(index+1)}. #{name}"  }
+  puts str if !arr.empty?
+end
+
+def take_a_number(arr,name)
+  arr << name
+  puts "Welcome, #{name}. You are number #{arr.index(name)+1} in line."
+end
+
+def now_serving(arr)
+  puts "There is nobody waiting to be served!" if 
+  "Currently serving Ada."
+end
